9113452f4f03318e7cb009c06babe0e681a3e286
Alireza Kamyab <alirezakamyab19@gmail.com>
2022-02-14 11:28:39 +0330

    Customer interation with database implementation

--- customer.py
+++ customer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import person
 import user
+import bookStore
+from databaseHelper import DatabaseHelper
 
 
 class CustomerException(ValueError):
@@ -19,6 +21,9 @@ class Customer(person.Person, user.User):
         if 'credit' in kwargs: self.credit = kwargs['credit']
         else: self.credit = 0
 
+        if 'store' in kwargs: self.store = kwargs['store']
+        else: self.store = None
+
         self._books = []
         self._completed = []
         person.Person.__init__(self, **kwargs)
@@ -30,21 +35,60 @@ class Customer(person.Person, user.User):
     def completed(self): return self._completed
     @property
     def credit(self): return self._credit
+    @property
+    def store(self): return self._store
 
     @credit.setter
     def credit(self, value):
         if value < 0: raise CustomerException("Credit cannot be negative value!")
         self._credit = value
 
-    def buyABook(self, book):
-        raise NotImplemented
+    @store.setter
+    def store(self, value): self._store = value
 
-    def setABookAsCompleted(self, book):
-        self.completed.append(book)
+    def buyABook(self, bk):
+        if bk.count <= 0: raise CustomerException("Book is out of stock!")
+        if bk.price > self.credit: raise CustomerException("Insufficient credit!")
+
+        # Update books table from database
+        book_table = DatabaseHelper(self.store.databasePath, self.store.BOOKS_TABLE)
+        bk.count -= 1
+        book_table.update(id=bk.id, count=bk.count)
+        book_table.close()
+
+        # Update Inventory table from database
+        inventory_table = DatabaseHelper(self.store.databasePath, self.store.BOOK_INVENTORY_TABLE)
+        inventory_table.insert(customer_id=self.id, book_id=bk.id, completed=False)
+        inventory_table.close()
+
+        self.credit -= bk.price
+        # Update customer table from database
+        customer_table = DatabaseHelper(self.store.databasePath, self.store.CUSTOMERS_TABLE)
+        customer_table.update(id=self.id, credit=self.credit)
+        customer_table.close()
+
+        # add to the list
+        self.books.append(bk)
+
+    def setABookAsCompleted(self, bk):
+        self.completed.append(bk)
+
+        # Update database
+        helper = DatabaseHelper(self.store.databasePath, self.store.BOOK_INVENTORY_TABLE)
+        lst = [helper.searchData(book_id=bk.id, customer_id=self.id)]
+        data = lst[0]
+        data['completed'] = True
+        helper.update(**data)
+        helper.close()
 
     def increaseCredit(self, amount):
         if amount < 0: raise CustomerException("Cannot increase a negative value")
         self.credit += amount
+
+        # Update database
+        helper = DatabaseHelper(self.store.databasePath, self.store.CUSTOMERS_TABLE)
+        helper.update(id=self.id, credit=self.credit)
+        helper.close()
 
 
 def main():
